1ed2dda0a0ca7d10cbbb32e979851c0078835011
vexiou <138394044+vexiou@users.noreply.github.com>
2023-11-05 19:13:12 +0000

    Update method.php

--- method.php
+++ method.php
@@ -15,6 +15,18 @@ if($conn->connect_error){
     echo"connection successful!";
 }
 
+//GET VALUE FROM FORM
+$email = $_POST ["email"];
+$password = $_POST ["password"];
+$firstname = $_POST ["firstname"];
+$lastname = $_POST ["lastname"];
+$username = $_POST ["username"];
+
+//QUERY TO INSERT VALUES TO DB
+$insertQuery = "INSERT INTO `users` (email,password,firstname,lastname,username) VALUES ($email,$password,$firstname,$lastname,$username)";
+
+$conn->query($insertQuery);
+
 // The SQL QUERY TO GET DATA FROM USER TABLE IN THE DB
 $sqlquery ="SELECT * FROM `users`";
 $result = $conn->query($sqlquery)
